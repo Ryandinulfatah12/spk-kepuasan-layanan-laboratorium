2fe7c333c93fba075e0e41cf6f9ed83ce54db503
Ryan Dinul Fatah <46431723+Ryandinulfatah12@users.noreply.github.com>
2024-05-01 00:25:01 +0700

    revisi 1

---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,4 @@
+<div style="display: flex; align-items: center;">
+    <img src="{{ asset('img/logo.png') }}" alt="Logo SKPL" style="width: 20%;">
+    <span style="margin-left: 10px;">SPK PL</span>
+</div>
